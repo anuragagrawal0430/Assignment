89fb121fb015b3f9dab741ebea89e4bce3335a3c
anurag <anuragagrawal3004@gmail.com>
2016-06-03 14:01:03 +0530

    Modified merge and delete argument. Also improved helpgit add .

--- Duplicatefile/dup1.sh
+++ Duplicatefile/dup1.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 
 if [ "$1" = "h" ]; then
-	printf "\n ./dup1.sh arg1 arg2 \n ./dup1.sh m <path>: for merging for files \n ./dup1.sh d <path> : for deleting of files\n"
+	printf "\n ./dup1.sh arg1 arg2 \n ./dup1.sh -m <path>: for merging for files \n ./dup1.sh -d <path> : for deleting of files\n Example:\n ./dup1.sh -m /home/user_name/ will merge all the files in /home/user_name directory \n ./dup1.sh -d /home/user_name/ will delete all the files in /home/user_name directory\n"
 
 
 elif [ -n "$2" ]; then
 answer=$(find "$2" -type f -printf "%f %s\n"|sort -k1,1nr -k2,2n|uniq -d| cut -d" " -f1 )
 
 
-if [ "$1" = "m" ]; then
+if [ "$1" = "-m" ]; then
 for i in $answer; 
 do 
-z=$(find . -name $i | xargs md5sum | cut -d" " -f1 | uniq -d)
+z=$(find "$2" -name $i | xargs md5sum | cut -d" " -f1 | uniq -d)
 k=$(find "$2" -type f -exec md5sum {} + | grep  $z | cut -d" " -f3 | head -n -1)
 for j in $k
 do
@@ -23,10 +23,10 @@ done
 printf "\nDone Mering of files\n"
 
 
-elif [ "$1" = "d" ]; then
+elif [ "$1" = "-d" ]; then
 for i in $answer; 
 do 
-z=$(find . -name $i | xargs md5sum | cut -d" " -f1 | uniq -d)
+z=$(find "$2" -name $i | xargs md5sum | cut -d" " -f1 | uniq -d)
 k=$(find "$2" -type f -exec md5sum {} + | grep  $z | cut -d" " -f3)
 for j in $k
 do
@@ -39,5 +39,5 @@ fi
 
 
 else
-	printf "\nInvild Arguments..Please type './dup1.sh h' for help \n"
+	printf "\nInvild Arguments! Please type './dup1.sh h' for more help. \n"
 fi
