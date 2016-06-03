95d6ff7a667dedae3f5497e62e97b7a17429cac6
anurag <anuragagrawal3004@gmail.com>
2016-06-03 20:23:54 +0530

    Duplicate files Shell Script Final Commit

--- Duplicatefile/dup.sh
+++ Duplicatefile/dup.sh
@@ -1,14 +1,26 @@
 #! /bin/bash
 
-if [ "$1" = "h" ]; then
-	printf "\n ./dup1.sh arg1 arg2 \n ./dup1.sh -m <path>: for merging for files \n ./dup1.sh -d <path> : for deleting of files\n Example:\n ./dup1.sh -m /home/user_name/ will merge all the files in /home/user_name directory \n ./dup1.sh -d /home/user_name/ will delete all the files in /home/user_name directory\n"
+#Group Name :: Juggernaut
+#Group ID :: 65
+#Problem Statement :: Write a program to list duplicate files from hard drive and give user option to remove them or merge them.
+#Assumptions :: In merging duplicate files, all the duplicates are merged into the last(tail) file.   
+if [ "$1" = "-h" ]; then
+	printf "\n ./dup.sh arg1 arg2\n ./dup.sh -l <path>: for listing of duplicate files \n ./dup.sh -m <path>: for merging of duplicate files \n ./dup.sh -d <path>: for deleting of duplicate files\n Example:\n ./dup.sh -l /home/username/ will list all the duplicate files in /home/username directory \n ./dup.sh -m /home/username/ will merge all the duplicate files in /home/username directory \n ./dup.sh -d /home/username/ will delete all the duplicate files in /home/username directory\n"
 
 
 elif [ -n "$2" ]; then
 answer=$(find "$2" -type f -printf "%f %s\n"|sort -k1,1nr -k2,2n|uniq -d| cut -d" " -f1 )
 
 
-if [ "$1" = "-m" ]; then
+if [ "$1" = "-l" ]; then
+for i in $answer; 
+do 
+find "$2" -name $i 
+printf "\n"
+done
+
+
+elif [ "$1" = "-m" ]; then
 for i in $answer; 
 do 
 z=$(find "$2" -name $i | xargs md5sum | cut -d" " -f1 | uniq -d)
@@ -39,5 +51,5 @@ fi
 
 
 else
-	printf "\nInvild Arguments! Please type './dup1.sh h' for more help. \n"
+	printf "\nInvild Arguments! Please type './dup.sh -h' for more help. \n"
 fi
